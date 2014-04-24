e2c669fbddd9e9e35ab8cab169c8d600a234f0d7
jonathangreco <nataniel.greco@gmail.com>
2014-04-24 11:25:03 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Génerer un module en ligne de commande</title>
+</head>
+<body>
+<form method="POST" action="lineCommand.php">
+	<label for="module">Nom du module</label>
+	<input type="texte" name="module"/>
+	<input type="submit" name="go"/>
+</form>
+</body>
+</html>
---
+++ lineCommand.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="width : 960px; margin: auto;">
+
+</body>
+</html>
+<?php
+
+if($_POST['module']) {
+	$str = $_POST['module'];
+	$string = "mkdir -pv ".ucfirst($str)."/{config,tests/".ucfirst($str)."Test/{Controller,Framework,Util},src/".ucfirst($str)."/{Controller,Form,Entity,Factory,Service},view/".$str."/".$str.",language}
+	|touch ".ucfirst($str)."/Module.php
+	|touch ".ucfirst($str)."/autoload_classmap.php
+	|touch ".ucfirst($str)."/config/module.config.php
+	|touch ".ucfirst($str)."/language/en_US.po
+	|touch ".ucfirst($str)."/language/fr_FR.po
+	|touch ".ucfirst($str)."/src/".ucfirst($str)."/Controller/".ucfirst($str)."Controller.php
+	|touch ".ucfirst($str)."/tests/".ucfirst($str)."Bootstrap.php
+	|touch ".ucfirst($str)."/tests/TestConfig.php
+	|touch ".ucfirst($str)."/tests/".ucfirst($str)."Test/Framework/TestCase.php
+	|touch ".ucfirst($str)."/tests/".ucfirst($str)."Test/Util/ServiceManagerFactory.php";
+
+	echo '--------------------------------------------------<br/>';
+	echo "<div style='width : 500px;'>";
+	echo $string;
+	echo "</div>";
+	echo '<br/>--------------------------------------------------';
+	echo '<br/>';
+}
